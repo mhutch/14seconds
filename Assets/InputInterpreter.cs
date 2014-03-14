ae59c2cced3425f52f2d7187a01239350cdcad86
Michael Hutchinson <m.j.hutchinson@gmail.com>
2014-03-14 09:23:15 -0600

    Make InputInterpreter state static so other scripts can access it

--- Assets/InputInterpreter.cs
+++ Assets/InputInterpreter.cs
@@ -1,43 +1,44 @@
 ﻿using UnityEngine;
+using System;
 
 public class InputInterpreter : MonoBehaviour
 {
 	//controls
-	const KeyCode KeyArmStretchLeft = KeyCode.Q;
-	const KeyCode KeyArmStretchRight = KeyCode.W;
-	const KeyCode KeyLegStretchLeft = KeyCode.A;
-	const KeyCode KeyLegStretchRight = KeyCode.S;
-	const KeyCode KeyArmLeftGrip = KeyCode.Z;
-	const KeyCode KeyArmRightGrip = KeyCode.X;
-
+	const KeyCode KeyArmStretchLeft = KeyCode.F;
+	const KeyCode KeyArmLeftGrip = KeyCode.D;
+	const KeyCode KeyLegStretchLeft = KeyCode.V;
+	const KeyCode KeyArmStretchRight = KeyCode.J;
+	const KeyCode KeyArmRightGrip = KeyCode.K;
+	const KeyCode KeyLegStretchRight = KeyCode.N;
+	
 	// constants
 	const float StartOxygenTime = 14f; //seconds
 	const float StretchSpeed = 1f; // units per second
 	const float StretchOxygenUse = 0.5f; // oxygen seconds per stretch second
-
+	
 	//arm/leg state
-	float armStretchLeft, armStretchRight, legStretchLeft, legStretchRight;
-	bool armGripLeft, armGripRight;
-
-	public float ArmStretchLeft { get { return armStretchLeft; } }
-	public float ArmStretchRight { get { return armStretchRight; } }
-	public float LegStretchLeft { get { return legStretchLeft; } }
-	public float LegStretchRight { get { return legStretchRight; } }
-	public bool ArmGripLeft { get { return armGripLeft; } }
-	public bool ArmGripRight { get { return armGripRight; } }
-
-	public float OxygenTime { get; private set; }
-
+	static float armStretchLeft, armStretchRight, legStretchLeft, legStretchRight;
+	static bool armGripLeft, armGripRight;
+	
+	public static float ArmStretchLeft { get { return armStretchLeft; } }
+	public static float ArmStretchRight { get { return armStretchRight; } }
+	public static float LegStretchLeft { get { return legStretchLeft; } }
+	public static float LegStretchRight { get { return legStretchRight; } }
+	public static bool ArmGripLeft { get { return armGripLeft; } }
+	public static bool ArmGripRight { get { return armGripRight; } }
+	
+	static public float OxygenTime { get; private set; }
+	
 	void Start ()
 	{
 		OxygenTime = StartOxygenTime;
 	}
-
+	
 	void UpdateGrip (KeyCode key, ref bool value)
 	{
 		value = Input.GetKey (key);
 	}
-
+	
 	void UpdateStretch (KeyCode key, ref float value)
 	{
 		if (Input.GetKey (key)) {
@@ -45,7 +46,7 @@ public class InputInterpreter : MonoBehaviour
 				var newValue = Mathf.Min (1f, value + StretchSpeed * Time.deltaTime);
 				var delta = newValue - value;
 				value = newValue;
-
+				
 				OxygenTime -= delta * StretchOxygenUse;
 			}
 		} else {
@@ -53,17 +54,17 @@ public class InputInterpreter : MonoBehaviour
 				var newValue = Mathf.Max (0f, value - StretchSpeed * Time.deltaTime);
 				var delta = newValue - value;
 				value = newValue;
-
+				
 				OxygenTime -= delta * StretchOxygenUse;
 			}
 		}
 	}
-
+	
 	void Update ()
 	{
 		UpdateGrip (KeyArmLeftGrip, ref armGripLeft);
 		UpdateGrip (KeyArmRightGrip, ref armGripRight);
-
+		
 		UpdateStretch (KeyArmStretchLeft, ref armStretchLeft);
 		UpdateStretch (KeyArmStretchRight, ref armStretchRight);
 		UpdateStretch (KeyLegStretchLeft, ref legStretchLeft);
