b6223bc7b1eaf00d75ceca2b47c668c9efd82c94
Michael Hutchinson <m.j.hutchinson@gmail.com>
2014-03-15 14:25:41 -0600

    Use sprite bg instead of skybox, and bounce arrow

---
+++ Assets/Bounce.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class Bounce : MonoBehaviour
+{
+	void Start () 
+	{
+
+	}
+
+	float time;
+
+	void Update () 
+	{
+		time += Time.deltaTime;
+		transform.Translate (0.02f * Mathf.Sin (time * 2f * Mathf.PI), 0f, 0f);
+	}
+}
