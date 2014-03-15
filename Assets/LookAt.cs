373a1f90cbae3303f25446b0991f84c3b690e971
Michael Hutchinson <m.j.hutchinson@gmail.com>
2014-03-14 22:19:59 -0600

    Add a missing file

---
+++ Assets/LookAt.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LookAt : MonoBehaviour 
+{
+	
+	public Transform targetObject;
+	
+	void Start () 
+	{
+		
+	}
+	
+	void Update () 
+	{
+		this.transform.LookAt(targetObject, Vector3.forward);
+	}
+}
